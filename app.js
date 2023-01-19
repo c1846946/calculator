64dc58e1ddfa42bd569879573b4017469e61e6ee
Chris V <119519387+c1846946@users.noreply.github.com>
2023-01-19 11:10:47 -0500

    Rework logic

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-let displayValue = "";
 let display = document.querySelector(".display");
-let firstNumber
+let inputValue = "";
+let first
 let op
-let secondNumber
-let operatorDisplay
-let equalDisplay
+let second
+
 
 
 function add(a,b) {
@@ -40,91 +39,106 @@ function operate(operator, a, b) {
     }
 }
 
-function pressNumber(evt) { //nums need to be a string
+function pressDigit(evt) { //nums need to be a string
     if(evt.target.id) num = evt.target.id;
     if (evt.key) num = evt.key;
-    if (num == "." && displayValue.includes(".")) return;
+    if (num == "." && inputValue.includes(".")) return;
     if (num == "b" || num == "Backspace") {
-        displayValue = displayValue.substring(0, displayValue.length - 1);
-    } else if(displayValue == "0")  {
-        displayValue = num;
-    }else {
-    displayValue += num;
+        inputValue = inputValue.substring(0, inputValue.length - 1);
+    } else {
+    inputValue += num;
     };
-    display.innerText = displayValue;
-    console.log("first " + firstNumber ,"\nsecond " + secondNumber ,"\nop " + op, "\ndisplay " + displayValue, "\noperatorDisplay " + operatorDisplay, "\nequalDisplay " + equalDisplay);
+    
+    display.innerText = inputValue;
+    console.log("first " + first ,"\nsecond " + second ,"\nop " + op, "\ninputValue " , inputValue);
 }
 
-function chooseOperator(evt) {
-    if (op == undefined) firstNumber = undefined;
-    if (firstNumber && displayValue) {
-        pressEquals();
-    } else if (!firstNumber){
+function pressOp(evt) {
+    if (!first) first = Number(inputValue);
+    if (first && op && inputValue) {
+        second = Number(inputValue);
+        inputValue = "";
+        if (performCalc() == "infinite") {
+            clear();
+            display.innerText = "DAFUQ?!";
+            return
+        } else{
+            //set first to result of calc and clear op and second
+            first = performCalc();
+            op = undefined;
+            second = undefined;
+            //display first
+            display.innerText = String(first);
+        };
         
-        firstNumber = Number(displayValue);
-    
-    let operatorDisplay = displayValue;
-    display.innerText = operatorDisplay;
-    displayValue = "";
+        
     }
+    
+    //set op
     if (evt.target.id) op = evt.target.id;
     if (evt.key) op = evt.key;
+    inputValue = ""
+    
     console.log(evt.target.id)
-    console.log("first " + firstNumber ,"\nsecond " + secondNumber ,"\nop " + op, "\ndisplay " + displayValue, "\noperatorDisplay " + operatorDisplay, "\nequalDisplay " + equalDisplay);
+    console.log("first " + first ,"\nsecond " + second ,"\nop " + op, "\ninputValue " , inputValue);
+}
+
+function performCalc() {
+    if ((op == "/") &&  (second == "0")) {
+        return "infinite";
+    } else {
+    return Math.round(operate(op, first, second) * 100)/100;
+    }
 }
 
 function pressEquals() {
-    //check for 0 denominator here
-    console.log("started\nfirstNumber:", firstNumber, "\ndisplayValue:", displayValue)
-    //if (!firstNumber || !displayValue) return;
-    secondNumber = Number(displayValue);
-    console.log("continued")
-    if ((op == "/") &&  (secondNumber == "0")) {
+    second = Number(inputValue);
+    inputValue = "";
+    if (performCalc() == "infinite") {
         clear();
-        display.innerText = "DAFUQ!?";
-    } else {
-    equalDisplay = String(Math.round(operate(op, firstNumber, secondNumber) * 100)/100);
-    display.innerText = equalDisplay;
-    displayValue = "";
-    op = undefined;
-    //make secondNumber undefined here???
-    
-    firstNumber = Number(equalDisplay);
-    
-    console.table(":\n","first " + firstNumber ,"\nsecond " + secondNumber ,"\nop " + op, "\ndisplay " + displayValue, "\noperatorDisplay " + operatorDisplay, "\nequalDisplay " + equalDisplay);
+        display.innerText = "DAFUQ?!"
+    } else{
+        display.innerText = performCalc();
+        first = performCalc();
+        second = undefined;
+        op = undefined;
     }
+    
+    console.log("first " + first ,"\nsecond " + second ,"\nop " + op, "\ninputValue " , inputValue);
+    
 }
 
 function clear() {
-    firstNumber = undefined;
+    first = undefined;
     op = undefined;
-    secondNumber = undefined;
-    displayValue = "0";
-    equalDisplay = undefined;
-    operatorDisplay = undefined;
-    display.innerText = displayValue
+    second = undefined;
+    inputValue = "";
+    display.innerText = "";
+    
+    
+    console.log("first " + first ,"\nsecond " + second ,"\nop " + op, "\ninputValue " , inputValue);
 }
 
 //add listeners for all the buttons
 document.querySelector(".clear-button").addEventListener("click", clear);
 //add ids to all the number and operator buttons
-document.getElementById("9").addEventListener("click", pressNumber);
-document.getElementById("8").addEventListener("click", pressNumber);
-document.getElementById("7").addEventListener("click", pressNumber);
-document.getElementById("6").addEventListener("click", pressNumber);
-document.getElementById("5").addEventListener("click", pressNumber);
-document.getElementById("4").addEventListener("click", pressNumber);
-document.getElementById("3").addEventListener("click", pressNumber);
-document.getElementById("2").addEventListener("click", pressNumber);
-document.getElementById("1").addEventListener("click", pressNumber);
-document.getElementById("0").addEventListener("click", pressNumber);
-document.getElementById(".").addEventListener("click", pressNumber);
-document.getElementById("b").addEventListener("click", pressNumber);
+document.getElementById("9").addEventListener("click", pressDigit);
+document.getElementById("8").addEventListener("click", pressDigit);
+document.getElementById("7").addEventListener("click", pressDigit);
+document.getElementById("6").addEventListener("click", pressDigit);
+document.getElementById("5").addEventListener("click", pressDigit);
+document.getElementById("4").addEventListener("click", pressDigit);
+document.getElementById("3").addEventListener("click", pressDigit);
+document.getElementById("2").addEventListener("click", pressDigit);
+document.getElementById("1").addEventListener("click", pressDigit);
+document.getElementById("0").addEventListener("click", pressDigit);
+document.getElementById(".").addEventListener("click", pressDigit);
+document.getElementById("b").addEventListener("click", pressDigit);
 
-document.getElementById("+").addEventListener("click", chooseOperator);
-document.getElementById("-").addEventListener("click", chooseOperator);
-document.getElementById("*").addEventListener("click", chooseOperator);
-document.getElementById("/").addEventListener("click", chooseOperator);
+document.getElementById("+").addEventListener("click", pressOp);
+document.getElementById("-").addEventListener("click", pressOp);
+document.getElementById("*").addEventListener("click", pressOp);
+document.getElementById("/").addEventListener("click", pressOp);
 
 document.querySelector(".equals-button").addEventListener("click", pressEquals);
 
@@ -132,9 +146,9 @@ document.addEventListener('keyup', filterUnusedKeys);
 
 function filterUnusedKeys(evt) {
     console.log(evt.key)
-    if(evt.key == "1" || evt.key == "2" || evt.key == "3" || evt.key == "4" || evt.key == "5" || evt.key == "6" || evt.key == "7" || evt.key == "8" || evt.key == "9" || evt.key == "0"  || evt.key == "." || evt.key == "b" || evt.key == "Backspace") pressNumber(evt);
+    if(evt.key == "1" || evt.key == "2" || evt.key == "3" || evt.key == "4" || evt.key == "5" || evt.key == "6" || evt.key == "7" || evt.key == "8" || evt.key == "9" || evt.key == "0"  || evt.key == "." || evt.key == "b" || evt.key == "Backspace") pressDigit(evt);
 
-    if(evt.key == "/" || evt.key == "*" || evt.key == "-" || evt.key == "+") chooseOperator(evt);
+    if(evt.key == "/" || evt.key == "*" || evt.key == "-" || evt.key == "+") pressOp(evt);
     
     if (evt.key == "Enter") pressEquals();
 
